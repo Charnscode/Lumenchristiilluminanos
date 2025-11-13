ee425ba506fe33b9b8b1f50e37f18ab668d9c507
AGBOTA-Stevens <agbotagu@gmail.com>
2025-11-13 21:09:22 +0100

    nouveau fichier : .env 	modifié :         Lumenchristiilluminanos/settings.py

--- Lumenchristiilluminanos/settings.py
+++ Lumenchristiilluminanos/settings.py
@@ -77,13 +77,30 @@ WSGI_APPLICATION = 'Lumenchristiilluminanos.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+load_dotenv()
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': os.getenv('DB_NAME'),
+        'USER': os.getenv('DB_USER'),
+        'PASSWORD': os.getenv('DB_PASSWORD'),
+        'PORT': '3306',
+        'HOST': 'localhost',
     }
 }
 
+
+# EMAIL configuration
+# https://docs.djangoproject.com
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_USE_SSL = False
+EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
+DEFAULT_FROM_EMAIL = os.getenv('EMAIL_HOST_USER')
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
