1768aa2ec1982af7d777659414819529bf2f46d4
AGBOTA-Stevens <agbotagu@gmail.com>
2025-11-24 19:24:32 +0100

    modifié :         LumenChristiilluminanos_app/__pycache__/urls.cpython-310.pyc 	modifié :         Lumenchristiilluminanos/__pycache__/settings.cpython-310.pyc 	modifié :         Lumenchristiilluminanos/__pycache__/urls.cpython-310.pyc 	modifié :         Lumenchristiilluminanos/settings.py 	modifié :         static/css/style1.css

--- Lumenchristiilluminanos/settings.py
+++ Lumenchristiilluminanos/settings.py
@@ -80,15 +80,10 @@ WSGI_APPLICATION = 'Lumenchristiilluminanos.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': os.getenv('DB_NAME'),
-        'USER': os.getenv('DB_USER'),
-        'PASSWORD': os.getenv('DB_PASSWORD'),
-        'PORT': '3306',
-        'HOST': 'localhost',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
 
 # EMAIL configuration
 # https://docs.djangoproject.com
